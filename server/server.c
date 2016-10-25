b1e34cb494f935f476e86bbae9c034bcdf8e10bd
Bertrand Mollinier Toublet <bertrand@bmt-online.org>
2016-10-24 22:14:43 -0700

    Switch to poll()-based implementation

--- server/server.c
+++ server/server.c
@@ -1,9 +1,11 @@
 #include "server.h"
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include <poll.h>
 #include <signal.h>
 #include <unistd.h>
 
@@ -65,38 +67,33 @@ exit_failure:
     return EXIT_FAILURE;
 }
 
-#ifndef FD_COPY
-#define FD_COPY(fdset_orig_, fdset_copy_) \
-    do { \
-        for (int i = 0; i < nfds; i++) { \
-            if (FD_ISSET(i, (fdset_orig_))) FD_SET(i, (fdset_copy_)); \
-        } \
-    } while(0)
-#endif /* ndef FD_COPY */
-
 int server_main_loop(struct server *s) {
     if (NULL == s) return EXIT_FAILURE;
     if (!s->success) return EXIT_FAILURE;
 
-    fd_set readfds;
-    FD_ZERO(&readfds);
-    FD_SET(s->sockfd, &readfds);
-    int nfds = s->sockfd + 1;
+#define MAX_NFDS    (100)
+    struct pollfd fds[MAX_NFDS] = { { 0 } };
+    nfds_t nfds = 0;
+
+    /* set socket fd */
+    fds[nfds].fd = s->sockfd;
+    fds[nfds].events = POLLIN;
+    nfds++;
 
     while (1) {
-        fd_set readfds_cpy;
-        FD_COPY(&readfds, &readfds_cpy);
-
-        int rc = select(nfds, &readfds_cpy, NULL, NULL, NULL);
+        int rc = poll(fds, nfds, -1);
         if (rc < 0) {
             perror("failed to select for sockets");
             goto exit_failure;
         }
+        assert(0 != rc);
 
         /* process ready fds */
-        for (int fd = 0; fd < nfds; fd++) {
-            if (FD_ISSET(fd, &readfds_cpy)) {
-                if (fd == s->sockfd) {
+        for (nfds_t i = 0; i < nfds; i++) {
+            if (0 != fds[i].revents) {
+                if (fds[i].fd == s->sockfd) {
+                    assert(POLLIN == fds[i].revents);
+
                     /* this is the bound socket: accept new connection */
                     struct sockaddr_un cli_addr;
                     socklen_t clilen = sizeof cli_addr;
@@ -106,26 +103,31 @@ int server_main_loop(struct server *s) {
                         goto exit_failure;
                     }
                     printf("accepted %d\n", new_fd);
-                    FD_SET(new_fd, &readfds);
-                    if (new_fd >= nfds) nfds = new_fd + 1;
+
+                    assert(nfds != MAX_NFDS);
+                    fds[nfds].fd = new_fd;
+                    fds[nfds].events = POLLIN;
+                    nfds++;
                 }
                 else {
+                    assert(POLLIN == fds[i].revents);
+
                     /* this is any other socket: read and ack */
                     char msg[256] = { 0 };
-                    ssize_t nread = read(fd, msg, sizeof msg - 1);
+                    ssize_t nread = read(fds[i].fd, msg, sizeof msg - 1);
                     if (nread < 0) {
                         perror("failed to read from socket");
-                        FD_CLR(fd, &readfds);
-                        close(fd);
+                        fds[i].events = 0;
+                        close(fds[i].fd);
                         continue;
                     }
                     printf("received: \"%s\"\n", msg);
 
-                    ssize_t nwritten = write(fd, "ACK", 4);
+                    ssize_t nwritten = write(fds[i].fd, "ACK", 4);
                     if (nwritten < 0) {
                         perror("failed to write to socket");
-                        FD_CLR(fd, &readfds);
-                        close(fd);
+                        fds[i].events = 0;
+                        close(fds[i].fd);
                         continue;
                     }
                 }
